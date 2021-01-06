894e1ed7b01653ea620038c11bc37f104e755f30
Kintoho <hohlov.aleksandr.a@gmail.com>
2021-01-06 22:02:04 +0500

    2.2

--- src/main/java/ru/ssau/tk/alexanderkhkh/FirstRep/arrays/Array.java
+++ src/main/java/ru/ssau/tk/alexanderkhkh/FirstRep/arrays/Array.java
@@ -2,10 +2,17 @@ package ru.ssau.tk.alexanderkhkh.FirstRep.arrays;
 
 public class Array {
     public static int[] firstArray(int size) {
-        int[] array = new int[size];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = 1;
+        int[] array1 = new int[size];
+        return array1;
+    }
+
+    public static int[] secondArray(int size){
+        int[] array2 = new int[size];
+        array2[0]=2;
+        for (int i = 1; i < array2.length-1; i++) {
+            array2[i] = 1;
         }
-        return array;
+        array2[array2.length-1]=2;
+        return array2;
     }
 }
--- src/test/java/ru/ssau/tk/alexanderkhkh/FirstRep/arrays/ArrayTest.java
+++ src/test/java/ru/ssau/tk/alexanderkhkh/FirstRep/arrays/ArrayTest.java
@@ -8,7 +8,14 @@ public class ArrayTest {
 
     @Test
     public void testArray() {
-        int[] array = Array.firstArray(5);
-        assertEquals(array.length, 5, 0.001);
+        int[] array1 = Array.firstArray(5);
+        assertEquals(array1.length, 5, 0.001);
+
+        int[] array2 = Array.secondArray(5);
+        assertEquals(array2.length, 5, 0.001);
+        assertEquals(array2[0], 2, 0.001);
+        assertEquals(array2[4], 2, 0.001);
+        assertEquals(array2[2], 1, 0.001);
+
     }
 }
